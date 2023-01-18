340e861b0e013ab247bd8acd9671197923479064
lemmonas <lem20016@byui.edu>
2023-01-18 13:47:31 -0700

    login using node

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require ("express");
 const app = express();
 
 const port = 3000;
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res)=>{
     res.send("Hello Andra");
+});
+
+app.post("/login", (req, res) =>{
+    const loginUser = req.body.userName;
+    console.log('Login username: '+loginUser);
+    res.send('Hello there, '+loginUser);
 });
 
 app.listen(port, ()=>{
