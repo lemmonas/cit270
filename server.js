4ac5a5f6aec0968c4c4412e9dc3147adfcaa2cc1
lemmonas <lem20016@byui.edu>
2023-02-27 13:39:16 -0700

    updated server file with new cert location

--- server.js
+++ server.js
@@ -80,9 +80,9 @@ app.post("/login", async (req, res) =>{
 // });
 
 https.createServer (
-    {key: fs.readFileSync('./server.key'),
-     cert: fs.readFileSync('./server.cert'),
-     ca: fs.readFileSync('./chain.pem')},
+    {key: fs.readFileSync('./etc/letsencrypt/live/andrahale.cit270.com/privkey.pem'),
+     cert: fs.readFileSync('./etc/letsencrypt/live/andrahale.cit270.com/cert.pem'),
+     ca: fs.readFileSync('./etc/letsencrypt/live/andrahale.cit270.com/fullchain.pem')},
     app
 ).listen(port, ()=>{
     redisClient.connect();
