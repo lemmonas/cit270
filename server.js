cc810ab690bebe7d6e5ba4138c88eb3f76ea4240
lemmonas <lem20016@byui.edu>
2023-03-27 13:14:52 -0600

    redis and cit270 pods working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 const Redis = require("redis");
 
-const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+const redisClient = Redis.createClient({url:"redis://default:8AebdCgC2YDW2U5h@redis-stedi-andra:6379"});
 
 const {v4: uuidv4} = require('uuid'); //universally unique identifier
 
